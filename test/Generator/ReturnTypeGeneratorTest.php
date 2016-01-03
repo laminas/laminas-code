02b4a43cb88b99c768cf7fefa7cab65694631f98
Marco Pivetta <ocramius@gmail.com>
2016-01-03 22:33:55 +0100

    zendframework/zend-code#29 - basic return type VO codegen assertions (for valid types)

--- test/Generator/ReturnTypeGeneratorTest.php
+++ test/Generator/ReturnTypeGeneratorTest.php
@@ -21,4 +21,58 @@ class ReturnTypeGeneratorTest extends \PHPUnit_Framework_TestCase
     {
         self::assertContains(GeneratorInterface::class, class_implements(ReturnTypeGenerator::class));
     }
+
+    /**
+     * @dataProvider validReturnTypeProvider
+     *
+     * @param string $typeString
+     * @param string $expectedReturnType
+     */
+    public function testFromValidReturnTypeString(string $typeString, string $expectedReturnType)
+    {
+        $generator = ReturnTypeGenerator::fromReturnTypeString($typeString);
+
+        self::assertSame($expectedReturnType, $generator->generate());
+    }
+
+    public function validReturnTypeProvider() : array
+    {
+        return [
+            ['foo', '\\foo'],
+            ['foo', '\\foo'],
+            ['foo1', '\\foo1'],
+            ['foo\\bar', '\\\foo\\bar'],
+            ['foo\\bar\\baz', '\\foo\\bar\\baz'],
+            ['foo\\bar\\baz1', '\\foo\\bar\\baz1'],
+            ['FOO', '\\FOO'],
+            ['FOO1', '\\FOO1'],
+            ['array', 'array'],
+            ['Array', 'array'],
+            ['ARRAY', 'array'],
+            ['callable', 'callable'],
+            ['Callable', 'callable'],
+            ['CALLABLE', 'callable'],
+            ['string', 'string'],
+            ['String', 'string'],
+            ['STRING', 'string'],
+            ['int', 'int'],
+            ['Int', 'int'],
+            ['INT', 'int'],
+            ['float', 'float'],
+            ['Float', 'float'],
+            ['FLOAT', 'float'],
+            ['bool', 'bool'],
+            ['Bool', 'bool'],
+            ['BOOL', 'bool'],
+            ['object', '\\object'],
+            ['Object', '\\Object'],
+            ['OBJECT', '\\Object'],
+            ['mixed', '\\mixed'],
+            ['Mixed', '\\Mixed'],
+            ['MIXED', '\\MIXED'],
+            ['resource', '\\resource'],
+            ['Resource', '\\Resource'],
+            ['RESOURCE', '\\RESOURCE'],
+        ];
+    }
 }
