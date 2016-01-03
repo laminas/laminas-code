624a033e5dfb2750e74be891026ae66e83346378
Marco Pivetta <ocramius@gmail.com>
2016-01-03 22:15:36 +0100

    zendframework/zend-code#29 - basic return type generator tests

---
+++ test/Generator/ReturnTypeGeneratorTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Code\Generator;
+
+use Zend\Code\Generator\GeneratorInterface;
+use Zend\Code\Generator\ReturnTypeGenerator;
+
+/**
+ * @covers \Zend\Code\Generator\ReturnTypeGenerator
+ */
+class ReturnTypeGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsAGenerator()
+    {
+        $this->assertTrue(class_implements(ReturnTypeGenerator::class, GeneratorInterface::class));
+    }
+}
