72b29c6aaf8630b2435b6c0d77d495dd1d0a379a
Bas Kamer <bas@bushbaby.nl>
2016-01-13 13:20:30 +0100

    name of a method cannot be optional

--- src/Generator/ClassGenerator.php
+++ src/Generator/ClassGenerator.php
@@ -719,7 +719,7 @@ class ClassGenerator extends AbstractGenerator
      * @return ClassGenerator
      */
     public function addMethod(
-        $name = null,
+        $name,
         array $parameters = [],
         $flags = MethodGenerator::FLAG_PUBLIC,
         $body = null,
