c2a11bbcd06042920649d0ab5164d7d3244105c8
mikeSimonson <mike.simonson@gmail.com>
2016-03-15 00:16:06 +0100

    Trying to fix a platform issue with windows line ending
    Uniformazing the line ending in the project to avoid issue
    with windows.
    
    Fix #45 and Fix #39

--- src/Reflection/DocBlock/Tag/LicenseTag.php
+++ src/Reflection/DocBlock/Tag/LicenseTag.php
@@ -69,6 +69,6 @@ class LicenseTag implements TagInterface
 
     public function __toString()
     {
-        return 'DocBlock Tag [ * @' . $this->getName() . ' ]' . PHP_EOL;
+        return 'DocBlock Tag [ * @' . $this->getName() . ' ]' . "\n";
     }
 }
