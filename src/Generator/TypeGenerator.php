4ff2b36cacc4b99bdd31137a56945a206849d863
Marco Pivetta <ocramius@gmail.com>
2016-08-19 19:34:05 +0200

    Removed useless nullability matcher

--- src/Generator/TypeGenerator.php
+++ src/Generator/TypeGenerator.php
@@ -39,7 +39,7 @@ final class TypeGenerator implements GeneratorInterface
     /**
      * @var string a regex pattern to match valid class names or types
      */
-    private static $validIdentifierMatcher = '/^\??[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*(\\\\[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*$/';
+    private static $validIdentifierMatcher = '/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*(\\\\[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*$/';
     // @codingStandardsIgnoreEnd
 
     /**
