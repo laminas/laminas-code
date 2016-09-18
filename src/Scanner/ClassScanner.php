430dd9005b4e4f3faa927f228240b087c28b3266
Mapkuff <map.siwapun@gmail.com>
2016-09-19 02:07:36 +0700

    Fix Travis code convention

--- src/Scanner/ClassScanner.php
+++ src/Scanner/ClassScanner.php
@@ -1011,8 +1011,7 @@ class ClassScanner implements ScannerInterface
 
                     case null:
                         if (($classContext == T_IMPLEMENTS && $tokenContent == ',')
-                            || ($classContext == T_EXTENDS && $tokenContent == ',' && $this->isInterface))
-                        {
+                            || ($classContext == T_EXTENDS && $tokenContent == ',' && $this->isInterface)) {
                             $classInterfaceIndex++;
                             $this->shortInterfaces[$classInterfaceIndex] = '';
                         }
