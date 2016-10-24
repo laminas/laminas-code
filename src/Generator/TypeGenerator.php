Ocramius/zend-code

Removed ignored coverage rule

src/Generator/TypeGenerator.php
@@ -35,12 +35,11 @@ final class TypeGenerator implements GeneratorInterface
      */
     private static $internalPhpTypes = ['void', 'int', 'float', 'string', 'bool', 'array', 'callable'];
 
-    // @codingStandardsIgnoreStart
     /**
      * @var string a regex pattern to match valid class names or types
      */
-    private static $validIdentifierMatcher = '/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(\\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*$/';
-    // @codingStandardsIgnoreEnd
+    private static $validIdentifierMatcher = '/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*'
+        . '(\\\\[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*$/';
 
     /**
      * @param string $type

laminas/laminas-code

src/Generator/TypeGenerator.php
@@ -35,12 +35,11 @@ final class TypeGenerator implements GeneratorInterface
      */
     private static $internalPhpTypes = ['void', 'int', 'float', 'string', 'bool', 'array', 'callable'];
 
-    // @codingStandardsIgnoreStart
     /**
      * @var string a regex pattern to match valid class names or types
      */
-    private static $validIdentifierMatcher = '/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*(\\\\[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*$/';
-    // @codingStandardsIgnoreEnd
+    private static $validIdentifierMatcher = '/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*'
+        . '(\\\\[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*$/';
 
     /**
      * @param string $type