1717836f531a6c4974cc897194c03100c498b27a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:25:17 -0600

    Rewriting as Laminas Project package

--- test/Reflection/DocBlockReflectionTest.php
+++ test/Reflection/DocBlockReflectionTest.php
@@ -1,34 +1,33 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-code for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-code/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-code/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Code\Reflection;
+namespace LaminasTest\Code\Reflection;
 
-use Zend\Code\Reflection\ClassReflection;
-use Zend\Code\Reflection\DocBlockReflection;
+use Laminas\Code\Reflection\ClassReflection;
+use Laminas\Code\Reflection\DocBlockReflection;
 
 /**
- * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @group      Zend_Reflection
- * @group      Zend_Reflection_DocBlock
+ * @copyright  Copyright (c) 2005-2013 Laminas (https://www.zend.com)
+ * @license    https://getlaminas.org/license/new-bsd     New BSD License
+ * @group      Laminas_Reflection
+ * @group      Laminas_Reflection_DocBlock
  */
 class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
 {
     public function testDocBlockShortDescription()
     {
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass5');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass5');
         $this->assertEquals('TestSampleClass5 DocBlock Short Desc', $classReflection->getDocBlock()->getShortDescription());
     }
 
     public function testDocBlockLongDescription()
     {
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass5');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass5');
         $expectedOutput = 'This is a long description for the docblock of this class, it should be longer than 3 lines. It indeed is longer than 3 lines now.';
 
 
@@ -38,7 +37,7 @@ class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
 
     public function testDocBlockTags()
     {
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass5');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass5');
 
         $this->assertEquals(3, count($classReflection->getDocBlock()->getTags()));
         $this->assertEquals(1, count($classReflection->getDocBlock()->getTags('author')));
@@ -46,17 +45,17 @@ class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(1, count($classReflection->getDocBlock()->getTags('method')));
 
         $methodTag = $classReflection->getDocBlock()->getTag('method');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\MethodTag', $methodTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\MethodTag', $methodTag);
 
         $propertyTag = $classReflection->getDocBlock()->getTag('property');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\PropertyTag', $propertyTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\PropertyTag', $propertyTag);
 
         $this->assertFalse($classReflection->getDocBlock()->getTag('version'));
 
         $this->assertTrue($classReflection->getMethod('doSomething')->getDocBlock()->hasTag('return'));
 
         $returnTag = $classReflection->getMethod('doSomething')->getDocBlock()->getTag('return');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\TagInterface', $returnTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\TagInterface', $returnTag);
         $this->assertEquals('mixed', $returnTag->getType());
 
 
@@ -64,7 +63,7 @@ class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
 
     public function testTabbedDocBlockTags()
     {
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass10');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass10');
 
         $this->assertEquals(3, count($classReflection->getDocBlock()->getTags()));
         $this->assertEquals(1, count($classReflection->getDocBlock()->getTags('author')));
@@ -72,17 +71,17 @@ class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(1, count($classReflection->getDocBlock()->getTags('method')));
 
         $methodTag = $classReflection->getDocBlock()->getTag('method');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\MethodTag', $methodTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\MethodTag', $methodTag);
 
         $propertyTag = $classReflection->getDocBlock()->getTag('property');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\PropertyTag', $propertyTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\PropertyTag', $propertyTag);
 
         $this->assertFalse($classReflection->getDocBlock()->getTag('version'));
 
         $this->assertTrue($classReflection->getMethod('doSomething')->getDocBlock()->hasTag('return'));
 
         $returnTag = $classReflection->getMethod('doSomething')->getDocBlock()->getTag('return');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\TagInterface', $returnTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\TagInterface', $returnTag);
         $this->assertEquals('mixed', $returnTag->getType());
     }
 
@@ -90,7 +89,7 @@ class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
     {
         //$this->markTestIncomplete('Line numbers incomplete');
 
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass5');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass5');
 
         $classDocBlock = $classReflection->getDocBlock();
 
@@ -101,7 +100,7 @@ class DocBlockReflectionTest extends \PHPUnit_Framework_TestCase
 
     public function testDocBlockContents()
     {
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass5');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass5');
 
         $classDocBlock = $classReflection->getDocBlock();
 
@@ -126,7 +125,7 @@ EOS;
 
     public function testToString()
     {
-        $classReflection = new ClassReflection('ZendTest\Code\Reflection\TestAsset\TestSampleClass5');
+        $classReflection = new ClassReflection('LaminasTest\Code\Reflection\TestAsset\TestSampleClass5');
 
         $classDocBlock = $classReflection->getDocBlock();
 
@@ -167,25 +166,25 @@ EOS;
         $this->assertEquals(1, count($docblockReflection->getTags('throws')));
 
         $returnTag = $docblockReflection->getTag('return');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\ReturnTag', $returnTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\ReturnTag', $returnTag);
         $this->assertEquals('int[]', $returnTag->getType());
         $this->assertEquals(array('int[]', 'null'), $returnTag->getTypes());
         $this->assertEquals('Description', $returnTag->getDescription());
 
         $throwsTag = $docblockReflection->getTag('throws');
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\ThrowsTag', $throwsTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\ThrowsTag', $throwsTag);
         $this->assertEquals('Exception', $throwsTag->getType());
 
         $paramTag = $paramTags[0];
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\ParamTag', $paramTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\ParamTag', $paramTag);
         $this->assertEquals('int', $paramTag->getType());
 
         $paramTag = $paramTags[1];
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\ParamTag', $paramTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\ParamTag', $paramTag);
         $this->assertEquals('int[]', $paramTag->getType());
 
         $paramTag = $paramTags[2];
-        $this->assertInstanceOf('Zend\Code\Reflection\DocBlock\Tag\ParamTag', $paramTag);
+        $this->assertInstanceOf('Laminas\Code\Reflection\DocBlock\Tag\ParamTag', $paramTag);
         $this->assertEquals('string', $paramTag->getType());
         $this->assertEquals(array('string', 'null'), $paramTag->getTypes());
     }
