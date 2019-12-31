25198eb107477ee011aff150fd33aabfdc1ce418
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:27:06 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/FooBarClass.php
+++ test/TestAsset/FooBarClass.php
@@ -1,10 +1,9 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-code for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-code/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-code/blob/master/LICENSE.md New BSD License
  */
 
 use ArrayObject;
@@ -14,6 +13,6 @@ use ArrayObject;
  * It is used to test, if the token scanner can find use
  * statements in classes with no namespace defined.
  */
-class ZendTest_Code_TestAsset_FooBar extends ArrayObject
+class LaminasTest_Code_TestAsset_FooBar extends ArrayObject
 {
 }
