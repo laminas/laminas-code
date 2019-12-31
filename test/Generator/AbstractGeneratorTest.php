e619b187582ea2b39b1da04125342c0b15a247d9
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:24:40 -0600

    Rewriting as Laminas Project package

--- test/Generator/AbstractGeneratorTest.php
+++ test/Generator/AbstractGeneratorTest.php
@@ -1,34 +1,32 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Code
+ * @see       https://github.com/laminas/laminas-code for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-code/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-code/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Code\Generator;
+namespace LaminasTest\Code\Generator;
 
 /**
- * @category   Zend
- * @package    Zend_Code_Generator
+ * @category   Laminas
+ * @package    Laminas_Code_Generator
  * @subpackage UnitTests
  *
- * @group Zend_Code_Generator
- * @group Zend_Code_Generator_Php
+ * @group Laminas_Code_Generator
+ * @group Laminas_Code_Generator_Php
  */
 class AbstractGeneratorTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructor()
     {
-        $generator = $this->getMockForAbstractClass('Zend\Code\Generator\AbstractGenerator', array(
+        $generator = $this->getMockForAbstractClass('Laminas\Code\Generator\AbstractGenerator', array(
             array(
                 'indentation' => 'foo',
             )
         ));
 
-        $this->assertInstanceOf('Zend\Code\Generator\GeneratorInterface', $generator);
+        $this->assertInstanceOf('Laminas\Code\Generator\GeneratorInterface', $generator);
         $this->assertEquals('foo', $generator->getIndentation());
     }
 }
