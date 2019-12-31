dc46a4338e5f566cbed4b844a3f7e096ef34d335
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:26:10 -0600

    Rewriting as Laminas Project package

--- test/Reflection/DocBlock/Tag/PropertyTagTest.php
+++ test/Reflection/DocBlock/Tag/PropertyTagTest.php
@@ -1,19 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-code for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-code/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-code/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Code\Reflection\DocBlock\Tag;
+namespace LaminasTest\Code\Reflection\DocBlock\Tag;
 
-use Zend\Code\Reflection\DocBlock\Tag\PropertyTag;
+use Laminas\Code\Reflection\DocBlock\Tag\PropertyTag;
 
 /**
- * @group      Zend_Reflection
- * @group      Zend_Reflection_DocBlock
+ * @group      Laminas_Reflection
+ * @group      Laminas_Reflection_DocBlock
  */
 class PropertyTagTest extends \PHPUnit_Framework_TestCase
 {
