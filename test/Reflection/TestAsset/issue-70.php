54ae1ff64af0501112662c3513fda132735ec94f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:05:08 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/Reflection/TestAsset/issue-70.php
+++ test/Reflection/TestAsset/issue-70.php
@@ -1,10 +1,4 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-code for the canonical source repository
- * @copyright https://github.com/laminas/laminas-code/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-code/blob/master/LICENSE.md New BSD License
- */
-
 spl_autoload_register(function () {
 });
