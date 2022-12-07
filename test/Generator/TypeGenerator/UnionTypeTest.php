7dcbb4d88a1c7c9100bc985c651a31327a6dbc0b
Marco Pivetta <ocramius@gmail.com>
2022-12-08 00:22:33 +0100

    Declaring a concept of `UnionType` containing sorted `AtomicType` and `IntersectionType` instances
    Signed-off-by: Marco Pivetta <ocramius@gmail.com>

---
+++ test/Generator/TypeGenerator/UnionTypeTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasTest\Code\Generator\TypeGenerator;
+
+use Laminas\Code\Generator\TypeGenerator\AtomicType;
+use Laminas\Code\Generator\TypeGenerator\IntersectionType;
+use Laminas\Code\Generator\TypeGenerator\UnionType;
+use PHPUnit\Framework\TestCase;
+
+/** @covers \Laminas\Code\Generator\TypeGenerator\UnionType */
+class UnionTypeTest extends TestCase
+{
+    /**
+     * @dataProvider sortingExamples
+     *
+     * @param AtomicType|IntersectionType $types
+     * @param non-empty-string            $expected
+     */
+    public function testTypeSorting(array $types, string $expected): void
+    {
+        self::assertSame(
+            $expected,
+            (new UnionType($types))->fullyQualifiedName()
+        );
+    }
+
+    /**
+     * @return non-empty-array<non-empty-string, array{non-empty-list<AtomicType|IntersectionType>, non-empty-string}>
+     */
+    public static function sortingExamples(): array
+    {
+        return [
+            'class types are sorted by name' => [
+                [
+                    AtomicType::fromString('\C'),
+                    AtomicType::fromString('A'),
+                    AtomicType::fromString('\B'),
+                    AtomicType::fromString('\D'),
+                ],
+                '\A|\B|\C|\D'
+            ],
+            'built-in types are moved to the end' => [
+                [
+                    AtomicType::fromString('iterable'),
+                    AtomicType::fromString('myIterator1'),
+                    AtomicType::fromString('myIterator2'),
+                ],
+                '\myIterator1|\myIterator2|iterable'
+            ],
+            'built-in types are kept at the end' => [
+                [
+                    AtomicType::fromString('myIterator2'),
+                    AtomicType::fromString('myIterator1'),
+                    AtomicType::fromString('iterable'),
+                ],
+                '\myIterator1|\myIterator2|iterable'
+            ],
+            'built-in types are sorted by priority' => [
+                [
+                    AtomicType::fromString('float'),
+                    AtomicType::fromString('int'),
+                    AtomicType::fromString('string'),
+                    AtomicType::fromString('null'),
+                    AtomicType::fromString('bool'),
+                ],
+                'bool|int|float|string|null'
+            ],
+            'intersection types are moved upfront' => [
+                [
+                    AtomicType::fromString('A'),
+                    AtomicType::fromString('D'),
+                    new IntersectionType([
+                        AtomicType::fromString('B'),
+                        AtomicType::fromString('C'),
+                    ]),
+                ],
+                '(\B&\C)|\A|\D'
+            ],
+            'intersection types are sorted by their own relative order' => [
+                [
+                    new IntersectionType([
+                        AtomicType::fromString('B'),
+                        AtomicType::fromString('C'),
+                    ]),
+                    new IntersectionType([
+                        AtomicType::fromString('A'),
+                        AtomicType::fromString('D'),
+                    ]),
+                ],
+                '(\A&\D)|(\B&\C)'
+            ],
+        ];
+    }
+}
